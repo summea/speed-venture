f857ceb6780646e9d8a97f81e30dd6677948c770
summea <chkmyemailnow@yahoo.com>
2014-04-15 10:40:10 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
 speed-venture
 =============
 
-learning how to speed up code
+speed-venture is all about learning how to speed up a given portion of code. In this case, the given portion of code focuses on having an outer and inner loop. The inner loop computes the sum of the numbers in a given array and the outer loop simply runs the inner loop _n_ times.
+
+## How to Compile
+
+    gcc -m32 -std=gnu99 -Wall -g speed-venture.c -o speed-venture
+
+## Example Output
+
+    // Mac OS X 10.8
+    $ time ./speed-venture
+
+    real  0m3.009s
+    user  0m3.005s
+    sys   0m0.003s
+
+    // Debian 3.2.54-2 i686 GNU/Linux
+    $ time ./speed-venture 
+
+    real  0m4.828s
+    user  0m4.756s
+    sys 0m0.060s
+
+## Notes
+
+- See LICENSE file for license information.
+- Original code template provided by mtrigobo
---
+++ speed-venture.c
@@ -0,0 +1,46 @@
+// original code template provided by mtrigobo
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// The code you submit must have these two values.
+#define N_TIMES     600000
+#define ARRAY_SIZE   10000
+
+int main (void)
+{
+    double  *array = calloc(ARRAY_SIZE, sizeof(double));
+    double  sum = 0;
+    int     i;
+
+    // You can add variables between this comment ...
+    int length = ARRAY_SIZE;
+    int limit = length-31;
+    double x0 = 0;
+    double x1 = 0;
+    double *ptr;
+    // ... and this one.
+
+    for (i = 0; i < N_TIMES; i++) {
+
+        // You can change anything between this comment ...
+        for (ptr = array; ptr < &array[limit]; ptr+=32) {
+            x0 += *ptr + *(ptr+2) + *(ptr+4) + *(ptr+6) + *(ptr+8) + *(ptr+10) + *(ptr+12) + *(ptr+14) + *(ptr+16) + *(ptr+18) + *(ptr+20) + *(ptr+22) + *(ptr+24) + *(ptr+26) + *(ptr+28) + *(ptr+30);
+            x1 += *(ptr+1) + *(ptr+3) + *(ptr+5) + *(ptr+7) + *(ptr+9) + *(ptr+11) + *(ptr+13) + *(ptr+15) + *(ptr+17) + *(ptr+19) + *(ptr+21) + *(ptr+23) + *(ptr+25) + *(ptr+27) + *(ptr+29) + *(ptr+31);
+        }
+
+        for (; ptr < &array[length]; ptr++)
+            x0 += *ptr; // add leftover items
+
+        sum = x0 + x1;  // important that this is just = assignment
+        // ... and this one. But your inner loop must do the same
+        // number of additions as this one does.
+
+        }
+
+    // You can add some final code between this comment ...
+    free(array);
+    // ... and this one.
+
+    return 0;
+}
